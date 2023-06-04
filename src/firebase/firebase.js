180720237272735d7fa4480c8f6b18ec9cf17090
nguyentungbach1509 <78577517+nguyentungbach1509@users.noreply.github.com>
2023-06-04 09:48:11 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKLQu9dPwt7Lrn6YwI6aBoIjkVMkzNywA",
-  authDomain: "tiktak-tiktok-clone-simple.firebaseapp.com",
-  projectId: "tiktak-tiktok-clone-simple",
-  storageBucket: "tiktak-tiktok-clone-simple.appspot.com",
-  messagingSenderId: "851887958668",
-  appId: "1:851887958668:web:2e2417f924f6c64f24a9a9",
-  measurementId: "G-JCCW0HLKRR"
+  apiKey: "AIzaSyDWu5OWBlyFRftaUnE1T2d28pRMMJdRuOA",
+  authDomain: "tiktak-tiktok-clone.firebaseapp.com",
+  projectId: "tiktak-tiktok-clone",
+  storageBucket: "tiktak-tiktok-clone.appspot.com",
+  messagingSenderId: "688914660119",
+  appId: "1:688914660119:web:c1dfd4e240e91764915552",
+  measurementId: "G-BP54K37PM7"
 };
 
   
